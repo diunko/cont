223842047fae1beeda1637bf8a43d02d87aa59ed
Dmitry Unkovsky <oil.crayons@gmail.com>
2013-04-11 22:56:34 +0300

    findfiles

--- test/basic.test.js
+++ test/basic.test.js
@@ -39,42 +39,141 @@ describe("basic promise usage", function() {
 
   it("2", function(done) {
 
-    lR(".", function(err, files) {
+    listR(".", function(err, files) {
+      if(err){
+        return done(err)}
       console.log("\n" + files.join("\n"));
-      done(err);
+      done()
     })
 
-    function lR(path, cb) {
-      var r = [];
 
-      fs.readdir(path, function(err, files) {
-        if (err) return cb(err);
-        var processed = 0;
 
-        listfiles = files.some(function(f) {
-          // var af = path + '/' + f;
-          fs.stat(f, function(err, stat) {
-            if (err) {
-              cb(err);
-            };
 
-            if (stat.isDirectory()) {
-              lR(f, function(err, f1) {
-                r.push(f1);
-              });
-            } else {
-              r.push(f);
-            }
+    function listR0(path,resultcb){
+      var dbg = 0
+      var toprocess = 1;
+      var r = []
 
-            processed++;
+      function error(err){
+        done(err)}
 
-            if (processed == files.length) {
-              cb(null, r);
-            }
+      function result(files){
+        console.log("================ all\n",files)
+        done()}
+      
+      function lR(path) {
+        
+        fs.readdir(path, function(err, files) {
+          if (err){
+            return error(err)}
+
+          dbg && console.log(path+":\n",files)
+          
+          toprocess += files.length;
+
+          files.some(function(f){
+            fs.stat(path+"/"+f,function(err,stat){
+              if(err){
+                return error(err)}
+              
+              if(stat.isDirectory()){
+                lR(path+"/"+f,function(err,alldirfiles){
+                  if(err){
+                    return error(err)}
+                  r=r.concat(alldirfiles.map(function(p){return path+"/"+p}))
+                  toprocess--;
+                  dbg && console.log("processed:",path+"/"+f)
+                  dbg && console.log("==== toprocess:",toprocess)
+                  if(toprocess == 0){
+                    return result(r)}
+                })
+              }else if(stat.isFile()){
+                r.push(path+"/"+f)
+                toprocess--;
+                dbg && console.log("processed:",path+"/"+f)
+                dbg && console.log("==== toprocess:",toprocess)
+                if(toprocess == 0){
+                  return result(r)}
+              }
+            })
           })
+
+          toprocess--;
+          if(toprocess == 0){
+            return result(r)}
         })
-      })
+      }
     }
+
+
+    function listR(path,resultcb){
+
+      var dbg = 0
+
+      var toprocess = 1
+      var result = []
+
+      _listR(path)
+
+      function _listR(path){
+
+        fs.readdir(path,onReaddir)
+        
+        function onReaddir(err,files){
+          if(err){
+            return onError(err)}
+          
+          toprocess += files.length
+
+          files.some(function(f){
+            dbg && console.log("stating file:",path+"/"+f)
+            fs.stat(path+"/"+f,statchecker(f))})
+          toprocess--
+          return check_done()}
+
+        function statchecker(f){
+          return function onStat(err,stat){
+            if(err){
+              return onError(err)}
+            var filepath = path+"/"+f
+
+            dbg && console.log("got stats for:",filepath)
+            dbg && console.log("and it is ",stat.isFile()?"a file":"something")
+            
+            if(stat.isDirectory()){
+              dbg && console.log("enter:",filepath)
+              _listR(path+"/"+f,onListR)
+            } else if(stat.isFile()){
+              result.push(filepath)
+              dbg && console.log("processed:",filepath)
+              toprocess--
+              return check_done()
+            }
+          }
+        }
+
+        function onListR(err,files){
+          if(err){
+            return error(err)}
+          result = result.concat(files)
+          toprocess--
+          return check_done()}
+        
+      }
+
+      function check_done(){
+        dbg && console.log("toprocess",toprocess)
+        if(toprocess == 0){
+          return resultcb(null,result)}
+      }
+
+      function onError(err){
+        return resultcb(err)
+      }
+      
+    }
+    
+
   })
 })
 
